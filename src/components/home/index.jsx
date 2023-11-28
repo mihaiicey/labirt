d6f3be3b61ccdbd883462d17ee6cf475bd1e6d44
mihaiicey <mihai@icey.ro>
2023-11-28 22:14:36 +0200

    .

---
+++ src/components/home/index.jsx
@@ -0,0 +1,6 @@
+export default function Home() {
+
+    return (
+        <main className="flex flex-col items-center justify-center min-h-screen space-y-20"><h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl">Vite, React, Tailwind, SupaBase minimal starter</h1><a href="https://github.com/moinulmoin/vite-react-tailwind-starter" target="_blank" rel="noreferrer"></a></main>
+    )
+}
