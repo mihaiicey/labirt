7c6ae28d28d97d99b76236974804d16c90f909cc
mihaiicey <mihai@icey.ro>
2023-11-29 15:08:34 +0200

    Update index.jsx

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -2,32 +2,32 @@
 export default function HomePage() {
   return (
     <section
-    style={{ backgroundImage: `url(${window.location.origin + '/hero1.jpg'})` }} className=' bgOverlay relative'>
-    <div
-      className="mx-auto max-w-screen-xl px-4 py-28 lg:flex lg:h-[90vh] lg:items-center relative"
-    >
-      <div className="mx-auto max-w-xl text-center">
-        <h1 className="text-3xl font-bold sm:text-4xl text-white">
-          <strong className="font-bold text-red-600  sm:block">
-            Tie foame sau pofta de o bere?
-          </strong>
+      style={{ backgroundImage: `url(${window.location.origin + '/hero1.jpg'})` }} className=' bgOverlay relative'>
+      <div
+        className="mx-auto max-w-screen-xl px-4 py-28 lg:flex lg:h-[90vh] lg:items-center relative"
+      >
+        <div className="mx-auto max-w-xl text-center">
+          <h1 className="text-3xl font-bold sm:text-4xl text-white">
+            <strong className="font-bold text-red-600  sm:block">
+              Ti-e foame sau pofta de o bere?
+            </strong>
             Dar nu ai gasit un local liber?
-        </h1>
-  
-        <p className="mt-4 sm:text-xl/relaxed text-white">
-        Rezerva o masa la restaurantul tau preferat, simplu si rapid.
-        </p>
-  
-        <div className="mt-8 flex justify-center">
-          <a
-            className="block w-full rounded bg-red-600 px-12 py-3 text-sm font-medium text-white shadow hover:bg-red-700 focus:outline-none focus:ring active:bg-red-500 sm:w-auto"
-            href="/login"
-          >
-            Intra in cont
-          </a>
+          </h1>
+
+          <p className="mt-4 sm:text-xl/relaxed text-white">
+            Rezerva o masa la restaurantul tau preferat, simplu si rapid.
+          </p>
+
+          <div className="mt-8 flex justify-center">
+            <a
+              className="block w-full rounded bg-red-600 px-12 py-3 text-sm font-medium text-white shadow hover:bg-red-700 focus:outline-none focus:ring active:bg-red-500 sm:w-auto"
+              href="/login"
+            >
+              Intra in cont
+            </a>
+          </div>
         </div>
       </div>
-    </div>
-  </section>
+    </section>
   );
 }
