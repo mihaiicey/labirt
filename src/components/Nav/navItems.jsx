382e5ec1be7effd4c40c68ab0ae760c877794ef3
mihaiicey <mihai@icey.ro>
2023-12-05 23:03:33 +0200

    Dev (#9)
    * Update App.jsx
    
    * Update CityDescription.jsx
    
    * update restaurante
    
    * tes1
    
    * Update index.jsx
    
    sorry git :(
    
    * error handler
    
    a
    
    * ,

--- src/components/Nav/navItems.jsx
+++ src/components/Nav/navItems.jsx
@@ -1,5 +1,5 @@
 export const navItems = [
     {name: 'Sibiu', link: 'sibiu', image: './cities/sibiu.jpg'},
     {name: 'Targu-Mures', link: 'targu-mures', image: './cities/targu-mures.jpg'},
-    {name: 'Cluj', link: 'cluj', image: ''}
+    // {name: 'Cluj', link: 'cluj', image: ''}
 ]
