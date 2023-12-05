9acd3467ddb76fc4295178189e4fc3a053fbd439
mihaiicey <mihai@icey.ro>
2023-12-05 18:35:03 +0200

    Update index.jsx

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -13,11 +13,9 @@ export default function Home() {
             </strong><br className="sm:hidden" />
             Dar nu ai gasit un local liber?
           </h1>
-
           <p className="mt-4 text-base sm:text-xl/relaxed text-white">
             Rezerva o masa la restaurantul tau preferat, simplu si rapid.
           </p>
-
           <div className="mt-8 flex justify-center">
             <a
               className="block w-full rounded bg-red-600 px-12 py-3 text-sm font-medium text-white shadow hover:bg-red-700 focus:outline-none focus:ring active:bg-red-500 sm:w-auto"
